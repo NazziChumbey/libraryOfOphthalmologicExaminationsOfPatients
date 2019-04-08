7b439513fb682b81e527041e75e82c3ca2b1491b
NazarChumbey <nazzichumbey@gmail.com>
2019-04-08 11:50:36 +0300

    Optimize import

---
+++ src/main/java/com/nazar/chumbey/libraryofophthalmologicexaminationsofpatients/LibraryofophthalmologicexaminationsofpatientsApplication.java
@@ -0,0 +1,13 @@
+package com.nazar.chumbey.libraryofophthalmologicexaminationsofpatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryofophthalmologicexaminationsofpatientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryofophthalmologicexaminationsofpatientsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nazar/chumbey/libraryofophthalmologicexaminationsofpatients/LibraryofophthalmologicexaminationsofpatientsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nazar.chumbey.libraryofophthalmologicexaminationsofpatients;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibraryofophthalmologicexaminationsofpatientsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
